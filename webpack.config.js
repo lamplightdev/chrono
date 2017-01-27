f3d3e3348c3181fccd74efd22d8f78478b32fe4d
Chris Haynes (mac) <chris.haynes@gmail.com>
2017-01-27 22:36:34 +0000

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,17 @@
-const webpack = require('webpack');
+// const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
   entry: './public/js/app.js',
-  devtool: 'cheap-eval-source-map',
+  // devtool: 'cheap-eval-source-map',
   output: {
     filename: 'app-dist.js',
     path: path.resolve(__dirname, './public/js'),
   },
   plugins: [
+    /*
     new webpack.optimize.UglifyJsPlugin({
     }),
+    */
   ],
 };
